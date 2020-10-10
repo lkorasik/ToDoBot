6a73eada8ff927c628dbe3f8f25c323e1208f755
Lev <lkorasik83@gmail.com>
2020-10-10 18:57:16 +0500

    Add echo bot

--- src/main/java/com/telegrambot/Bot.java
+++ src/main/java/com/telegrambot/Bot.java
@@ -1,4 +1,38 @@
 package com.telegrambot;
 
-public class Bot {
+import org.apache.http.cookie.SM;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot{
+    private final String Token = "1309073462:AAFZKxukxVkrvvVhnHbWUzbnnrvhMRO6k7M";
+    private final String BotUserName = "ToDoBot";
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage().setChatId(chat_id).setText(message_text);
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BotUserName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return Token;
+    }
 }
