6ceb2a8112d838244e93454f328c75054872cc99
Lev <lkorasik83@gmail.com>
2020-10-10 19:38:53 +0500

    Remove unused import

--- src/main/java/com/telegrambot/Bot.java
+++ src/main/java/com/telegrambot/Bot.java
@@ -1,6 +1,5 @@
 package com.telegrambot;
 
-import org.apache.http.cookie.SM;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
