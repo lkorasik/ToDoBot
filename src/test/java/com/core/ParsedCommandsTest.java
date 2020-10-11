512762b0e948ae7842ce1320fc8f1d9e5acb20d7
Lev <lkorasik83@gmail.com>
2020-10-11 17:09:35 +0500

    Add more test

---
+++ src/test/java/com/core/ParsedCommandsTest.java
@@ -0,0 +1,14 @@
+package com.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ParsedCommandsTest {
+    @Test
+    public void parsedCommandsTest(){
+        var pc = new ParsedCommand("A", "B");
+
+        Assert.assertEquals(pc.getCommand(), "A");
+        Assert.assertEquals(pc.getBody(), "B");
+    }
+}
