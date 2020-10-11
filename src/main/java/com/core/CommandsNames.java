36c26b6b9d540d4fa85b20bdd39c9d9bf8dc2c0f
Lev <lkorasik83@gmail.com>
2020-10-11 14:44:59 +0500

    Add commands consts

---
+++ src/main/java/com/core/CommandsNames.java
@@ -0,0 +1,10 @@
+package com.core;
+
+public class CommandsNames {
+    public static final String addTask = "/add";
+    public static final String deleteTask = "/del";
+    public static final String showTasks = "/show";
+    public static final String start = "/start";
+    public static final String stop = "/stop";
+    public static final String help = "/help";
+}
