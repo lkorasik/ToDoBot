f9b808b404e14ab0469da1c35e2db1ec09828dc0
Dmitry Podaruev <dmitry.podaruev@urfu.me>
2020-11-22 17:51:09 +0500

    Remove redundant id field in `User` class

--- src/main/java/com/core/User.java
+++ src/main/java/com/core/User.java
@@ -7,15 +7,14 @@ import java.util.List;
 
 public class User {
 
-    private String id;
     private List<Task> toDoTasks;
     private List<Task> completedTasks;
     private State fsmState;
 
-    public User(String userId){
+    public User(){
         toDoTasks = new ArrayList<>();
         completedTasks = new ArrayList<>();
-        id = userId;
+        fsmState = State.EP;
     }
 
     public State getFsmState() {
@@ -24,10 +23,6 @@ public class User {
 
     public void setFsmState(State fsmState) {
         this.fsmState = fsmState;
-    }
-
-    public String getId() {
-        return id;
     }
 
     public List<Task> getToDoTasks() {
@@ -52,12 +47,5 @@ public class User {
 
     public void addCompletedTask(Task task){
         this.completedTasks.add(task);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                '}';
     }
 }
