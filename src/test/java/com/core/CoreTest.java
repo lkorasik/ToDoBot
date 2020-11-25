61d33d7e867b315f5f37a013a42507391a08d4f6
Dmitry Podaruev <dmitry.podaruev@urfu.me>
2020-11-25 22:09:23 +0500

    Update CoreTest.java according to all new features

--- src/test/java/com/core/CoreTest.java
+++ src/test/java/com/core/CoreTest.java
@@ -1,6 +1,6 @@
 package com.core;
 
-import com.authentication.Authenticator;
+import com.fsm.State;
 import org.junit.*;
 import java.io.*;
 import java.util.Scanner;
@@ -11,64 +11,78 @@ import java.util.Scanner;
  */
 public class CoreTest {
     private Core core;
-    private String user;
-    private static final String test_filename = "testfile";
+    private String userId;
+    private static final String test_filename = "testfile.json";
 
     @Before
-    public void setUp(){
+    public void setUp() {
         core = new Core(test_filename);
-        Authenticator auth = new Authenticator();
-        auth.authenticate("user");
-        user = auth.getUserId();
+        userId = "user";
     }
 
 
     /**
-     * Удаление файла после прохождения каждого теста
+     * Удаление содержимого после каждого прохождения теста.
+     *
+     * Необходимо для того, чтобы результаты предудыщих тестов не накладывались на текущие.
      */
     @After
     public void tearDown() throws FileNotFoundException {
-        PrintWriter pw = new PrintWriter(test_filename);
+        var pw = new PrintWriter(test_filename);
         pw.close();
     }
 
     @AfterClass
-    public static void deleteFile(){
-        File testfile = new File(test_filename);
-        if (testfile.exists()){
+    public static void deleteFile() {
+        var testfile = new File(test_filename);
+        if (testfile.exists()) {
             testfile.delete();
         }
     }
 
     /**
-     * Проверка, что файл с данными пользователей создаеться и имеет в себе данные
+     * Проверка, что файл с данными пользователей создаеться и в него записываются все поля объекта класса User
      */
     @Test
     public void fileWithUsersIsCreatingTest() throws FileNotFoundException {
-        core.addTask(user, "DO");
+        var firstTaskDescription = "DO";
+        var secondTaskDescription = "SOMETHING";
+        core.createUser(userId);
+        core.addTask(userId, firstTaskDescription);
+        core.addTask(userId, secondTaskDescription);
+        core.setUserFSMState(userId, State.LISTEN);
         File f = new File(test_filename);
         Assert.assertTrue(f.isFile());
         Scanner scanner = new Scanner(f);
         String user_data = scanner.nextLine();
-        Assert.assertEquals("{\"user\":[{\"description\":\"DO\"}]}", user_data) ;
+        Assert.assertEquals(String.format("{\"%s\":{\"toDoTasks\":[{\"description\":\"%s\"},{\"description\":\"%s\"}],\"completedTasks\":[],\"fsmState\":\"LISTEN\"}}",
+                userId, firstTaskDescription, secondTaskDescription), user_data);
     }
 
 
     /**
-     * Проверка десереализцаии с JSON файла
+     * Проверка десереализцаии всех типов заданий пользователя с JSON файла
      */
     @Test
     public void uploadTasksFromJsonTest() {
         try {
-            File file = new File(test_filename);
+            var file = new File(test_filename);
+            var toDoTaskDescription = "do something";
+            var completedTaskDescription = "completed task";
             FileOutputStream fos = new FileOutputStream(file);
-            fos.write(String.format("{\"%s\":[{\"description\": \"Task1\"}]}", user).getBytes());
+            fos.write(String.format(
+                    "{\"%s\":{\"toDoTasks\":[{\"description\":\"%s\"}]," +
+                            "\"completedTasks\":[{\"description\":\"%s\"}]," +
+                            "\"fsmState\":\"NOTIFICATION\"}}",
+                    userId, toDoTaskDescription, completedTaskDescription).getBytes());
+            core = new Core(test_filename);
+            String formattedToDoTasks = core.getFormattedToDoTasks(userId);
+            String formattedCompletedTasks = core.getFormattedCompletedTasksString(userId);
+            Assert.assertEquals(String.format("Id\tОписание\n0\t%s", toDoTaskDescription), formattedToDoTasks);
+            Assert.assertEquals(String.format("Id\tОписание\n0\t%s", completedTaskDescription), formattedCompletedTasks);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        core = new Core(test_filename);
-        String result = core.getTasks(user);
-        Assert.assertEquals("Id\tОписание\n0\tTask1", result);
     }
 
     /**
@@ -78,40 +92,75 @@ public class CoreTest {
      */
     @Test
     public void deleteExistingTaskTest() throws IncorrectTaskIdTypeException, NotExistingTaskIndexException {
-        core.addTask(user,"First");
-        core.deleteTask(user,"0");
-        String result = core.getTasks(user);
-        Assert.assertEquals("Congratulations! You don't have any tasks yet", result);
+        core.createUser(userId);
+        core.addTask(userId, "test" );
+        core.deleteTask(userId, "0");
+        var result = core.getFormattedToDoTasks(userId);
+        Assert.assertEquals("Congratulations! You don't have any tasks!", result);
     }
 
     /**
-     * Попытка удаления задачи, Id которой не записан в списке задач
+     * Попытка удаления несуществующей задачи
      * @throws IncorrectTaskIdTypeException при попытке удаления задачи с некорректным Id
      * @throws NotExistingTaskIndexException при попытке удаления несуществующей задачи
      */
     @Test(expected = NotExistingTaskIndexException.class)
     public void deleteNotExistingTaskTest() throws IncorrectTaskIdTypeException, NotExistingTaskIndexException {
-        core.addTask(user, "Do");
-        core.deleteTask(user,"2");
+        core.createUser(userId);
+        core.addTask(userId, "Do");
+        core.deleteTask(userId,"2");
     }
 
     /**
      * Попытка удаления задачи с указанием некорректного Id
+     *
      * @throws IncorrectTaskIdTypeException при попытке удаления задачи с некорректным Id
      * @throws NotExistingTaskIndexException при попытке удаления несуществующей задачи
      */
     @Test(expected = IncorrectTaskIdTypeException.class)
     public void deleteTaskWithIncorrectArgumentTest() throws IncorrectTaskIdTypeException, NotExistingTaskIndexException {
-        core.deleteTask(user,"do");
+        core.createUser(userId);
+        core.deleteTask(userId,"do");
+    }
+
+
+    /**
+     * Проверка, что при отметке задачи как выполненной, она удаляется из списка на выполнение и
+     * добавляется в список выполненных
+     *
+     * @throws NotExistingTaskIndexException при попытке удаления задачи с некорректным Id
+     * @throws IncorrectTaskIdTypeException при попытке удаления несуществующей задачи
+     */
+    @Test
+    public void completeTaskTest() throws NotExistingTaskIndexException, IncorrectTaskIdTypeException {
+        var taskDescription = "do something";
+        core.createUser(userId);
+        core.addTask(userId, taskDescription);
+        core.completeTask(userId, "0");
+        var todoTasks = core.getFormattedToDoTasks(userId);
+        var completedTasks = core.getFormattedCompletedTasksString(userId);
+        Assert.assertEquals("Congratulations! You don't have any tasks!", todoTasks);
+        Assert.assertEquals(String.format("Id\tОписание\n0\t%s", taskDescription), completedTasks);
     }
 
     /**
      * Проверка списка задач без добавления в него чего-либо
      */
     @Test
-    public void showEmptyTaskListTest(){
-        String result = core.getTasks(user);
-        Assert.assertEquals("Congratulations! You don't have any tasks yet", result);
+    public void showEmptyTodoTaskListTest(){
+        core.createUser(userId);
+        String result = core.getFormattedToDoTasks(userId);
+        Assert.assertEquals("Congratulations! You don't have any tasks!", result);
+    }
+
+    /**
+     * Проверка списка выполненных задач без совершения операций над задачами
+     */
+    @Test
+    public void showEmptyCompletedTaskList(){
+        core.createUser(userId);
+        var result = core.getFormattedCompletedTasksString(userId);
+        Assert.assertEquals("You haven't done any tasks yet", result);
     }
 
     /**
@@ -119,21 +168,46 @@ public class CoreTest {
      */
     @Test
     public void showNotEmptyTaskListTest(){
-        core.addTask(user,"Do something");
-        String result = core.getTasks(user);
-        Assert.assertEquals("Id\tОписание\n0\tDo something", result);
+        var taskDescription = "Do something";
+        core.createUser(userId);
+        core.addTask(userId,taskDescription);
+        String result = core.getFormattedToDoTasks(userId);
+        Assert.assertEquals(String.format("Id\tОписание\n0\t%s", taskDescription), result);
     }
+
 
     /**
      * Проверка того, что у каждого пользователя свои и только свои задачи
      */
     @Test
     public void separatedTasksByUsersTest(){
-        core.addTask(user, "Do work");
-        core.addTask("newUser", "Do another work");
-        String firstUserResult = core.getTasks(user);
-        String secondUserResult = core.getTasks("newUser");
+        var firstUserName = userId;
+        var secondUserName = "newUser";
+        core.createUser(firstUserName);
+        core.createUser(secondUserName);
+        core.addTask(firstUserName, "Do work");
+        core.addTask(secondUserName, "Do another work");
+        var firstUserResult = core.getFormattedToDoTasks(firstUserName);
+        var secondUserResult = core.getFormattedToDoTasks(secondUserName);
         Assert.assertEquals("Id\tОписание\n0\tDo work", firstUserResult);
         Assert.assertEquals("Id\tОписание\n0\tDo another work", secondUserResult);
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void clearTaskListsTest() throws NotExistingTaskIndexException, IncorrectTaskIdTypeException {
+        core.createUser(userId);
+        var firstTaskDescription = "first task";
+        var secondTaskDescription = "second task";
+        core.addTask(userId, firstTaskDescription);
+        core.addTask(userId, secondTaskDescription);
+        core.completeTask(userId,"1");
+        core.clearAllTaskLists(userId);
+        var formattedTodoTasks = core.getFormattedToDoTasks(userId);
+        Assert.assertEquals("Congratulations! You don't have any tasks!", formattedTodoTasks);
+        var formattedCompletedTasks = core.getFormattedCompletedTasksString(userId);
+        Assert.assertEquals("You haven't done any tasks yet", formattedCompletedTasks);
     }
 }
