0cbfbd7c0a8da218ec14ee2d8f39c675b88f63fb
Dmitry Podaruev <dmitry.podaruev@urfu.me>
2020-12-01 10:30:53 +0500

    Add StartConsoleBot.java

---
+++ src/main/java/com/consolebot/StartConsoleBot.java
@@ -0,0 +1,24 @@
+package com.consolebot;
+
+import com.core.Constants;
+import java.text.ParseException;
+import java.util.Scanner;
+
+/**
+ * Класс, который запускает консольную версию бота
+ * @author Dmitry
+ */
+public class StartConsoleBot {
+
+    public static void main(String[] args) throws ParseException {
+        var scanner = new Scanner(System.in);
+        var userId = "";
+        while (userId.equals("")){
+            System.out.println(Constants.LOGIN_MESSAGE);
+            userId = scanner.nextLine();
+        }
+        var bot = new ConsoleBot(userId);
+        bot.printGreetingsMessage();
+        bot.run();
+    }
+ }
